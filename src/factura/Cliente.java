29778f9b4bbb487f90b5a13a4fd5b789438a3b16
ferchuqui <luisfernandofarji@hotmail.com>
2017-06-21 11:29:21 -0300

    modificado

--- src/factura/Cliente.java
+++ src/factura/Cliente.java
@@ -15,7 +15,7 @@ public class Cliente {
     public String getNombre() {
         return nombre;
     }
-
+//modificando para ver que pasa
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
